1bc6a01d34014c4879b14afb271c664455a14362
biaodigit <15622285015@163.com>
2020-06-26 17:35:43 +0800

    feat:product detail page

--- src/views/product_detail/index.tsx
+++ src/views/product_detail/index.tsx
@@ -1,9 +1,10 @@
 import * as React from "react"
+import ProductOverview from "./components/product_overview"
 import "./index.scss"
 
 class ProductDetail extends React.Component<{},{}> {
     public render() {
-        return <div>detail</div>
+        return <ProductOverview/>
     }
 }
 
